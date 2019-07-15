fe522adf528431d707669522bd3f3387851a0a2f
Melissa Stock <melissastock44@gmail.com>
2019-07-15 14:46:12 -0700

    refactored

--- front-end/component/History/History.js
+++ front-end/component/History/History.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Header from '../Header/Header';
+
 export default class History extends React.Component{
   static navigationOptions = { title: 'History' };
   render(){
   return(
+    <>
+    <Header />
     <View>
       <Text>
         ALOHA
       </Text>
     </View>
+    </>
   );
 }
 }
