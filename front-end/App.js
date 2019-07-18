b5d196386c08c5266983fdd600782d916bb071f9
Jag <jagdeepsingh3939@gmail.com>
2019-07-17 20:20:30 -0700

    removed unused files

--- front-end/App.js
+++ front-end/App.js
@@ -3,13 +3,9 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import CreateGoal from './src/component/Create-Goal/Create-Goal';
 import Dashboard from './src/component/Dashboard/Dashboard';
-import Header from './src/component/Header/Header';
-import History from './src/component/History/History';
 import HomePage from './src/component/Home-Page/Home-Page';
 
 const mainNavigator = createStackNavigator({
-    Header:   { screen: Header },
-    History:  { screen: History },
     HomePage: { screen: HomePage },
     Dashboard: { screen: Dashboard },
     CreateGoal: { screen: CreateGoal },
